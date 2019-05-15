939b97653c9c106e98083245019253f23e9eb9da
Softwarica College Lab <softwarica@softwarica.com>
2019-05-15 11:47:50 +0545

    Upload Image

--- app/src/main/java/com/example/apiconsumption/EmployeeClient.java
+++ app/src/main/java/com/example/apiconsumption/EmployeeClient.java
@@ -2,10 +2,15 @@ package com.example.apiconsumption;
 
 import java.util.List;
 
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface EmployeeClient {
@@ -20,4 +25,11 @@ public interface EmployeeClient {
     Call<Employee>createEmployee(
             @Body Employee employee
     );
+
+    @Multipart
+    @POST("3/upload")
+    Call<ResponseBody> upload(
+            @Header("Authorization") String clientID,
+            @Part() MultipartBody.Part file
+            );
 }
--- app/src/main/java/com/example/apiconsumption/MainActivity.java
+++ app/src/main/java/com/example/apiconsumption/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.apiconsumption;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,9 +10,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.List;
+import java.io.ByteArrayOutputStream;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -19,7 +26,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button show, add;
+    Button show, add, update;
     EditText etId;
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -111,5 +118,48 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+
+
+        update = findViewById(R.id.btnUpdate);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bitmap bm = BitmapFactory.decodeResource(MainActivity.this.getResources(),R.drawable.deadpool);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bm.compress(Bitmap.CompressFormat.JPEG,100,stream);
+                byte[] byteArray = stream.toByteArray();
+
+                MultipartBody.Part body = MultipartBody.Part.createFormData(
+                        "image",
+                        "deadpool",
+                        RequestBody.create(MediaType.parse("image/*"), byteArray)
+                );
+
+                String BASE_URL = "https://api.imgur.com/";
+                OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+                httpClient.addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY));
+
+                Retrofit.Builder builder =new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create());
+                Retrofit retrofit = builder.client(httpClient.build()).build();
+                EmployeeClient employeeClient = retrofit.create(EmployeeClient.class);
+                Call<ResponseBody> call = employeeClient.upload("Client-ID 6803e8b51da1ab7",body);
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        Log.d(TAG, response.toString());
+
+                        Toast.makeText(MainActivity.this,"Image Uploaded" , Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                        Log.d(TAG, t.getLocalizedMessage());
+                    }
+                });
+            }
+        });
+
     }
 }
